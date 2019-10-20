cb2f097a88c9ff29758dbb1d45e54eff0cedcb41
Siga Vaitiekutė <siga.vaitiekute@ktu.edu>
2019-10-20 18:15:53 +0300

    Feature-variable-shout įkėlimas į Github

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 import sys
-
-
-def greet(name: str): 
-    return f"Hello, {name}!"
-    
-    
-if __name__== "__main__":
-    print(greet(sys.argv[1]))
+ 
+def greet(greeted_name: str, shout_count: int = 1):
+    return f"Hello, {greeted_name}{shout_count * '!'}"
+   
+if __name__ == "__main__":
+    n = int(sys.argv[2]) if len(sys.argv) == 3 else 1
+    print(greet(sys.argv[1], n))
